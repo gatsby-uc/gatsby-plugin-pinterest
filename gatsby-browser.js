d3cb5342aecfdba2a3186b529e0c9d1d7f3ab48d
Michaël De Boey <info@michaeldeboey.be>
2020-02-27 18:11:32 +0100

    Add linting + fix ESLint errors

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,16 +2,16 @@ const injectPinterestScript = (tall, round) => {
   function addJS() {
     const s = document.createElement(`script`);
     s.type = `text/javascript`;
-    s.setAttribute("async", "async");
-    s.setAttribute("defer", "defer");
-      s.setAttribute("data-pin-hover", "true");
+    s.setAttribute('async', 'async');
+    s.setAttribute('defer', 'defer');
+      s.setAttribute('data-pin-hover', 'true');
     if(tall) {
-      s.setAttribute("data-pin-tall", "true");
+      s.setAttribute('data-pin-tall', 'true');
     }
     if(round) {
-      s.setAttribute("data-pin-round", "true");
+      s.setAttribute('data-pin-round', 'true');
     }
-    s.setAttribute("src", "https://assets.pinterest.com/js/pinit.js");
+    s.setAttribute('src', 'https://assets.pinterest.com/js/pinit.js');
     document.getElementsByTagName(`head`)[0].appendChild(s);
   }
   addJS();
