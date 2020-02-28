9e1dda1b4cc74c8844b7a6e94c21455d0f81e5bd
Michaël De Boey <info@michaeldeboey.be>
2020-02-28 18:14:34 +0100

    Don't show bookmark button by default
    Closes #4
    Closes #5
    
    BREAKING CHANGE: Bookmark button isn't enabled by default anymore

--- src/gatsby-browser.js
+++ src/gatsby-browser.js
@@ -1,18 +1,22 @@
-const injectPinterestScript = (tall, round) => {
+const injectPinterestScript = ({ saveButton = false }) => {
   const addJS = () => {
     const script = document.createElement("script");
     script.async = true;
     script.defer = true;
     script.src = "https://assets.pinterest.com/js/pinit.js";
 
-    script.setAttribute("data-pin-hover", "true");
+    if (Boolean(saveButton)) {
+      script.setAttribute("data-pin-hover", "true");
 
-    if (tall) {
-      script.setAttribute("data-pin-tall", "true");
-    }
+      const { round = false, tall = true } =
+        typeof saveButton === "boolean" ? {} : saveButton;
+      if (round) {
+        script.setAttribute("data-pin-round", "true");
+      }
 
-    if (round) {
-      script.setAttribute("data-pin-round", "true");
+      if (tall) {
+        script.setAttribute("data-pin-tall", "true");
+      }
     }
 
     document.getElementsByTagName("head")[0].appendChild(script);
@@ -23,10 +27,17 @@ const injectPinterestScript = (tall, round) => {
 
 let injectedPinterestScript = false;
 
-exports.onRouteUpdate = (args, { tall = true, round = false } = {}) => {
-  if (document.querySelector("[data-pin-do]") !== null) {
+exports.onRouteUpdate = (args, pluginOptions = {}) => {
+  const querySelectors = [
+    "[data-pin-do]",
+    Boolean(pluginOptions.saveButton) ? "img" : "",
+  ]
+    .filter(Boolean)
+    .join(",");
+
+  if (document.querySelector(querySelectors) !== null) {
     if (!injectedPinterestScript) {
-      injectPinterestScript(tall, round);
+      injectPinterestScript(pluginOptions);
 
       injectedPinterestScript = true;
     }
